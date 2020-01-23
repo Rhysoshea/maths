7015161881f0dab585598b6ef082ae4b8f2020f4
rhys <rhys.shea.10@ucl.ac.uk>
2020-01-23 12:57:15 +0900

    improvements

--- gbm_paths.py
+++ gbm_paths.py
@@ -17,36 +17,25 @@ def gbm_sims(n_sim, T, dt, mu, sigma, S0):
     m = (mu - 0.5 * sigma*sigma) * dt
     S = S0 * np.ones((n_sim, T+1))
     BM = sigma * math.sqrt(dt) * np.random.randn(n_sim, T)
-    # print (S)
-    # exit()
-    for r in range(len(BM)):
-        BM[r] = np.cumsum(m + BM[r])
 
-    print (BM)
-    # exit()
     for r in range(len(S)):
         for t in range(len(S[r])-1):
-            S[r][t+1] = S0 * math.exp(BM[r][t-1])
-    # S[:, 1:] = S0 * math.exp(np.cumsum(m+BM, axis=1))
+            S[r][t+1] = S[r][t]* math.exp(BM[r][t])
+
     return S
 
 def plotter(data):
     f, ax = plt.subplots(figsize=(10,10))
     for row in data:
-        # print (row)
         plt.plot(range(len(row)), row)
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-
     plt.show()
 
-n_sim = 5
+n_sim = 10
 T = 100
-dt = 0.1
-mu = 0
-sigma = 10
-S0 = 100
+dt = 0.01
+mu = 0.5
+sigma = 1
+S0 = 10
 
 result = gbm_sims(n_sim, T, dt, mu, sigma, S0)
-print (result)
 plotter(result)
